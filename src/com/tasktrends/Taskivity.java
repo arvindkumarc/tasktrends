e668280958ef15c9cf3ecb322db536e8b2f8a88e
Arvind <arvindc@thoughtworks.com>
2011-07-20 17:28:36 +0530

    Changed the idea

--- src/com/tasktrends/Taskivity.java
+++ src/com/tasktrends/Taskivity.java
@@ -1,19 +1,18 @@
 package com.tasktrends;
 
 import android.app.Activity;
-import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import com.tasktrends.dao.DataHelper;
 import com.tasktrends.utils.DateTime;
 
 public class Taskivity extends Activity {
     private Button createTask;
     private EditText taskName;
-    private TextView taskDate;
     private TextView taskTime;
     private DataHelper dbHelper;
 
@@ -21,19 +20,6 @@ public class Taskivity extends Activity {
     private static final int TIME_DIALOG_ID = 1;
 
     private DateTime dateTime;
-
-    private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
-        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-            dateTime.setDate(year, monthOfYear, dayOfMonth);
-            updateDisplay();
-        }
-    };
-    private TimePickerDialog.OnTimeSetListener mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
-        public void onTimeSet(TimePicker timePicker, int hour, int minute) {
-            dateTime.setTime(hour, minute);
-            updateDisplay();
-        }
-    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,7 +33,6 @@ public class Taskivity extends Activity {
         dateTime = new DateTime();
         createTask = (Button) findViewById(R.id.create_task_button);
         taskName = (EditText) findViewById(R.id.task_name);
-        taskDate = (TextView) findViewById(R.id.task_date);
         taskTime = (TextView) findViewById(R.id.task_time);
 
         taskName.setOnClickListener(new View.OnClickListener() {
@@ -60,14 +45,8 @@ public class Taskivity extends Activity {
 
         createTask.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                dbHelper.insert(taskName.getText().toString(), taskDate.getText().toString());
+//                dbHelper.insert(taskName.getText().toString(), );
                 Toast.makeText(getApplicationContext(), "Task Created", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        taskDate.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                showDialog(DATE_DIALOG_ID);
             }
         });
 
@@ -76,24 +55,5 @@ public class Taskivity extends Activity {
                 showDialog(TIME_DIALOG_ID);
             }
         });
-
-        updateDisplay();
-    }
-
-    private void updateDisplay() {
-        taskDate.setText(dateTime.getDate());
-        taskTime.setText(dateTime.getTime());
-    }
-
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        switch (id) {
-            case DATE_DIALOG_ID:
-                return new DatePickerDialog(this, mDateSetListener,
-                     dateTime.getYear(), dateTime.getMonthToDisplay(), dateTime.getDay());
-            case TIME_DIALOG_ID:
-                return new TimePickerDialog(this, mTimeSetListener, dateTime.getHour(), dateTime.getMinute(), true);
-        }
-        return null;
     }
 }
